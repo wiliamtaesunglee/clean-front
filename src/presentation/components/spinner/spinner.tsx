cb8e0d3a3eab44ca496af3fd13aa764b89f35c87
Wil.i.am <wiliamlee@sas.com.br>
2021-03-13 11:18:40 -0300

    feat: add login view

---
+++ src/presentation/components/spinner/spinner.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Styles from './spinner-styles.scss'
+
+const Spinner: React.FC = () => (
+  <div className={Styles.spinner}>
+    <div />
+    <div />
+    <div />
+  </div>
+)
+
+export default Spinner
--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,9 @@ module.exports = {
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.ts', '.tsx', 'js', 'scss'],
+    extensions: ['.ts', '.tsx', '.js', '.scss'],
     alias: {
-      '@': path.join(__dirname, 'src')
+      '~': path.join(__dirname, 'src')
     }
   },
   module: {
