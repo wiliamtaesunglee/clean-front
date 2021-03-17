5e9d77eb4364bf89959f0e25c9f73fa97a7b1cbf
guts <wiliam.tae@gmail.com>
2021-03-17 19:36:33 -0300

    test: ensure Login present valid state if pemail is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -69,4 +69,14 @@ describe('Login Component', () => {
     expect(passwordStatus.title).toBe('Tudo certo!')
     expect(passwordStatus.textContent).toBe('😀')
   })
+
+  test('Should show valid email state if validation succeeds', () => {
+    const { sut, validationStub } = makeSut()
+    validationStub.errorMessage = null
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+    const emailStatus = sut.getByTestId('email-status')
+    expect(emailStatus.title).toBe('Tudo certo!')
+    expect(emailStatus.textContent).toBe('😀')
+  })
 })
