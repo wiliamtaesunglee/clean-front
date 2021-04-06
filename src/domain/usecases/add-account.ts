17162b58f30a9b31b6db31fe61f91c14101e3aba
guts <wiliam.tae@gmail.com>
2021-04-06 07:28:13 -0300

    feat: add AddAccount to index

--- src/domain/usecases/add-account.ts
+++ src/domain/usecases/add-account.ts
@@ -1,11 +1,11 @@
 import { AccountModel } from '~/domain/models'
 
-export type AuthenticationParams = {
+export type AddAccountParams = {
   name: string
   email: string
   password: string
   passwrodConfirmation: string
 }
-export interface Authentication {
-  auth: (params: AuthenticationParams) => Promise<AccountModel>
+export interface AddAccount {
+  auth: (params: AddAccountParams) => Promise<AccountModel>
 }
